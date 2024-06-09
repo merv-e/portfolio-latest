e337d34d85a3d3ee9bbaff0214cbcfbcdc3aeb45
merv-e <34655454+merv-e@users.noreply.github.com>
2024-06-09 22:11:21 +0200

    Navbar is built Readme is deleted.

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  background-color: #00274d;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+`;
+
+const NavLinks = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 20px;
+`;
+
+const NavLink = styled(Link)`
+  color: #ffffff;
+  text-decoration: none;
+  transition: color 0.3s;
+
+  &:hover {
+    color: #5bc0eb;
+  }
+`;
+
+const Header = () => (
+  <Nav>
+    <NavLinks>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/projects">Projects</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact</NavLink>
+      </li>
+    </NavLinks>
+  </Nav>
+);
+
+export default Header;
