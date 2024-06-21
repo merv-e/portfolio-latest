84eb5204d66662b8c2db1bbd6b3c66eb475c62ca
merv-e <34655454+merv-e@users.noreply.github.com>
2024-06-21 13:52:29 +0200

    - more info about the project is added to data. - name of the folder is changed to utils.

---
+++ src/utils/data.js
@@ -0,0 +1,128 @@
+import contFusionBistro from "../assets/screenshots/continental-fusion-bistro.jpg";
+import brewery from "../assets/screenshots/brewery.png";
+import movieFinder from "../assets/screenshots/movie-finder.jpg";
+import empPollApp from "../assets/screenshots/employee-poll-app.png";
+import myReads from "../assets/screenshots/my-reads-app.png";
+import FurniFlare from "../assets/screenshots/furniFlare.jpg";
+import uncodebar from "../assets/screenshots/uncodebar.png";
+import markdownPreviewer from "../assets/screenshots/markdown-previewer.jpg";
+
+export const bio = {
+  header: "Hello, I'm Merve",
+  para: " Frontend Developer passionate about creating interactive and responsive web applications.",
+};
+
+export const projects = [
+  {
+    id: "uncodebar",
+    img_src: uncodebar,
+    title: "Uncodebar",
+    description: "uncodebar",
+    tech_stack: "HTML5, TailwindCSS",
+    url: "https://uncodebar.com/",
+    githubLink: "https://github.com/codebar/uncodebar-microsite",
+  },
+  {
+    id: "brewery",
+    img_src: brewery,
+    title: "Brewery",
+    description: "Brewery",
+    tech_stack: "HTML5, CSS3",
+    url: "https://merv-e.github.io/brewery/",
+    githubLink: "https://github.com/merv-e/brewery",
+  },
+  {
+    id: "furni-flare-store-app",
+    img_src: FurniFlare,
+    title: "FurniFlare",
+    description: "FurniFlare",
+    tech_stack: "React, Redux toolkit, React Router, TailwindCSS, DailyUI",
+    url: "",
+    githubLink: "",
+  },
+  // {
+  //   id: "adopt-a-pet",
+  //   img_src: AdoptAPet,
+  //   title: "adopt-a-pet",
+  //   description: "adopt-a-pet",
+  //   tech_stack: "React, React Router",
+  // url: ""
+  // },
+  // {
+  //   id: "counter-app",
+  //   img_src: counterApp,
+  //   title: "counter-app",
+  //   description: "counter-app",
+  //   tech_stack: "React, Redux toolkit",
+  // url: "",
+  // githubLink: "",
+  // },
+  {
+    id: "cont_fusion_bistro",
+    img_src: contFusionBistro,
+    title: "Continental Fusion Bistro",
+    description:
+      "This application allows you to seamlessly add and remove meals from your cart, mimicking the experience of ordering food online. Once you have added the desired meals, you can review your selections by clicking on 'Your Cart'. To explore more, click on the image.",
+    tech_stack: "React, CSS, Firebase",
+    url: "",
+    githubLink: "",
+  },
+  {
+    id: "movie_finder",
+    img_src: movieFinder,
+    title: "MovieFinder",
+    description:
+      "MovieFinder is a straightforward application designed to search for movies using a user-input search query. The underlying API powering this project is The Movie DB. To explore more, click on the image.",
+    tech_stack: "HTML, CSS, JavaScript",
+    url: "https://merv-e.github.io/MovieFinder/",
+    githubLink: "",
+  },
+  {
+    id: "my-reads",
+    img_src: myReads,
+    title: "MyReads",
+    description:
+      "Keep track of your books with this web app by simply adding your books to relevant shelves.",
+    tech_stack: "React",
+    url: "https://my-booooks.netlify.app/",
+    githubLink: "",
+  },
+  {
+    id: "employee_poll_app",
+    img_src: empPollApp,
+    title: "Employee Poll App",
+    description:
+      "Would you like your employers to work more efficiently as a team while having fun? It's possible with this app! To have the option to create, vote polls etc?",
+    tech_stack: "Bootstrap, React, Redux, Jest",
+    url: "",
+    githubLink: "",
+  },
+  {
+    id: "markdown-previewer",
+    img_src: markdownPreviewer,
+    title: "markdown-previewer",
+    description: "markdown-previewer",
+    tech_stack: "markdown-previewer",
+    url: "https://codepen.io/merv-e/full/QWrepbm",
+    githubLink: "",
+  },
+  {
+    id: "academic-nest",
+    img_src: "academic-nest",
+    title: "academic-nest",
+    description: "academic-nest",
+    tech_stack: "React",
+    url: "https://academic-nest.netlify.app/",
+    githubLink: "https://github.com/merv-e/academic-nest",
+  },
+
+  // {
+  //   id: "",
+  //   img_src: "",
+  //   title: "",
+  //   description: "",
+  //   tech_stack: "",
+  // url: "",
+  // githubLink: "",
+  // },
+];
