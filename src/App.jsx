1a28f7335dfa3b57b48c234164aaf9b6f1974413
merv-e <34655454+merv-e@users.noreply.github.com>
2024-07-19 14:48:15 +0200

    the pages that are not used are also deleted from router as well as the file that was exported by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
-import { Home, About, Projects, Contact } from "./pages";
+import { Home, Projects } from "./pages";
 import { Footer, Header } from "./components";
 
 function App() {
@@ -15,9 +15,7 @@ function App() {
         <main style={{ flex: 1 }}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
         <Footer />
